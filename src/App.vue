34339370be815b6005d618d80c33f8cd1fd9247c
alecasentini <ale.casentini@gmail.com>
2023-06-28 16:58:45 +0200

    Creazione e stampa componente ProjectCard

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script >
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 
 export default {
+  name: "App",
+  components: {
+    ProjectCard
+  },
+
   data() {
     return {
       projects: [],
@@ -40,23 +46,7 @@ export default {
   <div class="container">
     <div class="row">
       <div class="col-6 " v-for="(elem, index) in  projects " :key="index">
-        <div class="card">
-          <img class="card-img-top" :src="`${baseUrl}/storage/${elem.cover_image}`" alt="">
-          <div class="card-body">
-            <h4 class="card-title">{{ elem.name }}</h4>
-            <p class="card-text">{{ elem.description }}</p>
-            <div>
-              <h5>Type</h5>
-              <span v-if="elem.type">{{ elem.type.name }}</span>
-            </div>
-            <div>
-              <h5>Technologies</h5>
-              <ul v-if="elem.technologies">
-                <li v-for="(elem, index) in elem.technologies" :key="index">{{ elem.name }}</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <ProjectCard :elem="elem" />
       </div>
     </div>
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,36 @@
+<script>
+
+
+export default {
+    name: "ProjectCard",
+
+    props: ['elem'],
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000',
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img class="card-img-top" :src="`${baseUrl}/storage/${elem.cover_image}`" alt="">
+        <div class="card-body">
+            <h4 class="card-title">{{ elem.name }}</h4>
+            <p class="card-text">{{ elem.description }}</p>
+            <div>
+                <h5>Type</h5>
+                <span v-if="elem.type">{{ elem.type.name }}</span>
+            </div>
+            <div>
+                <h5>Technologies</h5>
+                <ul v-if="elem.technologies">
+                    <li v-for="(elem, index) in elem.technologies" :key="index">{{ elem.name }}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss"></style>
