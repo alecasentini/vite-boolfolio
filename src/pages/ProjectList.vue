4377d8e0e926b6206c609bad574ebd97948db3c6
alecasentini <ale.casentini@gmail.com>
2023-06-30 15:54:24 +0200

    Aggiunto filtraggio Types

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,5 +1,5 @@
 <script >
-import axios from 'axios';
+import axios, { all } from 'axios';
 import ProjectCard from '../components/ProjectCard.vue';
 
 export default {
@@ -13,32 +13,49 @@ export default {
             projects: [],
             baseUrl: 'http://127.0.0.1:8000',
             currentPage: 1,
-            lastPage: null
+            lastPage: null,
+            types: null,
+            selectedType: "all"
 
         }
     },
 
     mounted() {
         this.getProjects(1);
+        this.getTypes();
     },
 
     methods: {
         getProjects(projectApiPage, prevPage, nextPage) {
+
+
             if (prevPage && this.currentPage === 1) {
                 projectApiPage = this.lastPage;
             } else if (nextPage && this.currentPage === this.lastPage) {
                 projectApiPage = 1;
             }
+
+            const params = {
+                page: projectApiPage,
+            }
+
+            if (this.selectedType !== 'all') {
+                params.category_id = this.selectedType
+            }
+
             axios.get(`${this.baseUrl}/api/projects`, {
-                params: {
-                    page: projectApiPage
-                }
+                params
+            }).then(res => {
+                this.projects = res.data.projects.data
+                this.currentPage = res.data.projects.current_page
+                this.lastPage = res.data.projects.last_page
             })
-                .then(res => {
-                    this.projects = res.data.projects.data
-                    this.currentPage = res.data.projects.current_page
-                    this.lastPage = res.data.projects.last_page
-                })
+        },
+
+        getTypes() {
+            axios.get(`${this.baseUrl}/api/types`).then(res => {
+                this.types = res.data.types
+            })
         }
     }
 
@@ -49,6 +66,18 @@ export default {
     <h1>Projects</h1>
 
     <div class="container">
+
+        <div class="mb-3">
+            <label for="" class="form-label">Types Filter</label>
+
+            <select @change="getProjects()" v-model="selectedType" class="form-select form-select-lg" name="" id="">
+                <option value="all">-- All --</option>
+                <option :value="elem.id" v-for="(elem, index) in types" :key="index" selected>{{ elem.name }}</option>
+
+            </select>
+        </div>
+
+
         <div class="row">
             <div class="col-6 " v-for="(elem, index) in  projects " :key="index">
                 <ProjectCard :elem="elem" />
